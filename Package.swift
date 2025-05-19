15380b073ff2cd1fdbfd8fddecced207bb2c299c
Keen <zhekeen90@gmail.com>
2025-05-19 09:24:55 +0700

    Update Package

--- Package.swift
+++ Package.swift
@@ -1,14 +1,21 @@
-// swift-tools-version:5.0
-
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "SwiftReorder",
-    platforms: [.iOS(.v9)],
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
-        .library(name: "SwiftReorder", targets: ["SwiftReorder"])
+        .library(
+            name: "SwiftReorder",
+            targets: ["SwiftReorder"]
+        ),
     ],
     targets: [
-        .target(name: "SwiftReorder", path: "Source")
+        .target(
+            name: "SwiftReorder",
+            path: "Source"
+        )
     ]
 )
